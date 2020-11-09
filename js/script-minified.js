84aa792c958ae23eee69e5491b78d3617ee57a50
Visionary1810 <71036993+Visionary1810@users.noreply.github.com>
2020-11-09 21:37:21 +0300

    Final-3

---
+++ js/script-minified.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{var e=document.querySelector(".button-to-find-hotel"),t=document.querySelector(".pop-up");e.addEventListener("click",function(e){e.preventDefault(e),t.classList.contains("show")?t.classList.remove("show"):t.classList.add("show")})});
--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var searchButton = document.querySelector(".button-to-find-hotel");
-  var popup = document.querySelector(".pop-up");
-
-  searchButton.addEventListener("click", function (event) {
-    event.preventDefault(event);
-    
-    if (popup.classList.contains("show")) {
-      popup.classList.remove("show");
-    } else {
-      popup.classList.add("show");
-    }
+    var searchButton = document.querySelector(".button-to-find-hotel");
+    var popup = document.querySelector(".pop-up");
+  
+    searchButton.addEventListener("click", function (event) {
+      event.preventDefault(event);
+      
+      if (popup.classList.contains("show")) {
+        popup.classList.remove("show");
+      } else {
+        popup.classList.add("show");
+      }
+    });
   });
-});
